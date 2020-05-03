4feb69fca9199f63a8b5d9a820899ec7a4481a28
Julia Catalano <jcatalano1990@gmail.com>
2020-05-03 13:44:19 -0400

    feat: added a google map

--- client/components/catMap.js
+++ client/components/catMap.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {getAllCatsThunk} from '../store/cat'
+import {GoogleApiWrapper, InfoWindow, Map, Marker} from 'google-maps-react'
 
 class CatMap extends React.Component {
   constructor(props) {
@@ -12,10 +13,25 @@ class CatMap extends React.Component {
   }
 
   render() {
+    const style = {
+      width: '300px',
+      height: '300px'
+    }
     return (
       <div>
         <h1>local spots</h1>
         <div>{this.props.cats.map(cat => <h2>{cat.name}</h2>)}</div>
+        <div className="map">
+          <Map
+            google={this.props.google}
+            zoom={10}
+            initialCenter={{
+              lat: 40.6600615,
+              lng: -73.9532596
+            }}
+            style={style}
+          />
+        </div>
       </div>
     )
   }
@@ -29,4 +45,6 @@ const mapDispatchToProps = dispatch => ({
   getAllCats: () => dispatch(getAllCatsThunk())
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(CatMap)
+export default connect(mapStateToProps, mapDispatchToProps)(
+  GoogleApiWrapper({apiKey: 'AIzaSyA1LJsAQck-ITerB6JBStl9lLhxa5frpck'})(CatMap)
+)
