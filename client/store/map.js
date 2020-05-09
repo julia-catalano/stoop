eb8dc1fc3a4d0c93c5a5f639876d03d72008e193
Julia Catalano <jcatalano1990@gmail.com>
2020-05-09 16:10:02 -0400

    fix: map styling

--- client/store/map.js
+++ client/store/map.js
@@ -4,8 +4,8 @@ const SHIFT_VIEW = 'SHIFT_VIEW'
 //INITIAL STATE
 const initialMapState = {
   viewport: {
-    width: '100vw',
-    height: '100vh',
+    width: '50vw',
+    height: '50vh',
     latitude: 40.6600615,
     longitude: -73.9532596,
     zoom: 16
